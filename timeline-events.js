ebad655cff7922a1e6a2a242f9e0f5f809cbaae4
bkallencf <benjaminryokallen@gmail.com>
2025-07-30 03:45:29 -0700

    Mouseover test 2

--- timeline-events.js
+++ timeline-events.js
@@ -2,14 +2,23 @@
 const buttons = document.getElementsByClassName("button");
 const tooltip = document.getElementById("tooltip");
 
+// Content of each tooltip
+const tooltipContent = {
+    medicine: <p>Medicine Lodge Treaty (1867)</p>
+};
+
 for (let button of buttons) {
     button.addEventListener("mouseover", function(event) {
-        if (button.id === "medicine") {
-            tooltip.textContent = "The Medicine Lodge Treaty was signed in 1867."
+        const content = tooltipContent[button.id];
+        if (content) {
+            tooltip.innerHTML = content;
+            tooltip.style.display = "block";
+            tooltip.style.left = (event.pageX + 10) + "px";
+            tooltip.style.top = (event.pageY + 10) + "px";
         }
+    });
 
-        tooltip.style.display = "block";
-        tooltip.style.left = (event.pageX + 10) + "px";
-        tooltip.style.top = (event.pageY + 10) + "px";
+    button.addEventListener("mouseout", function () {
+        tooltip.style.display = "none";
     });
 };
