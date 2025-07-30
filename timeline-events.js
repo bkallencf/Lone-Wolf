dc9a38f3e9f760407aa4dfc56c9f04352bcf417b
bkallencf <benjaminryokallen@gmail.com>
2025-07-30 03:37:32 -0700

    Mouseover test

--- timeline-events.js
+++ timeline-events.js
@@ -1,2 +1,15 @@
 // Timeline Buttons
-const medicineButton = document.getElementById("medicine")
+const buttons = document.getElementsByClassName("button");
+const tooltip = document.getElementById("tooltip");
+
+for (let button of buttons) {
+    button.addEventListener("mouseover", function(event) {
+        if (button.id === "medicine") {
+            tooltip.textContent = "The Medicine Lodge Treaty was signed in 1867."
+        }
+
+        tooltip.style.display = "block";
+        tooltip.style.left = (event.pageX + 10) + "px";
+        tooltip.style.top = (event.pageY + 10) + "px";
+    });
+};
