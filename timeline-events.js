ba0e19e2097f7eb8f74708ff88ee95f0f1718c31
bkallencf <benjaminryokallen@gmail.com>
2025-08-02 09:37:33 -0700

    Timeline overhaul & bibliography

--- timeline-events.js
+++ timeline-events.js
@@ -1,19 +1,31 @@
 // Timeline Buttons
-const buttons = document.getElementsByClassName("button");
+const nodes = document.getElementsByClassName("node");
 const tooltip = document.getElementById("tooltip");
 
+// TODO: write the actual contents of each item
 // Content of each tooltip
 const tooltipContent = {
-    medicine: "Medicine Lodge Treaty (1867)",
-    dawes: "General Allotment Act (1887)",
-    jerome: "Jerome Commission and the Treaty of 1892 (1892)",
-    forthall: "An Act to Ratify an Agreement... (1900)",
-    allotment: "Land Seizures (1902)",
-    lonewolf: "Lone Wolf v. Hitchcock (1903)"
+    // Use ol for numbered or ul for bullet point
+    medicine: `<h3>The Medicine Lodge Treaty</h3>
+                <p>On [month] [date], the Kiowa, [list of others] signed a treaty with the US. Notably, the treaty included the following provisions:</p>
+                <ul>
+                    <li>Article II stated [stuff]</li>
+                    <li>Article IV</li>
+                    <li>Article XII</li>
+                </ul>
+                <img id="medicinePic" src="images/medicine.jpg></img>">`,
+    dawes: `<h3>The General Allotment Act</h3>`,
+    jerome: `<h3>The Jerome Commission</h3>`,
+    fortHall: `<h3>Congress Ratifies the Treaty</h3>
+                <p>An Act to Ratify an Agreement Made with the Indians of the Fort Hall Indian Reservation in Idaho, and Making an Appropriation to Carry the Same into Effect</p>`,
+    // TODO: 1901?
+    allotment: `<h3>Land Seizures</h3>`,
+    loneWolf: `<h3><em>Lone Wolf v. Hitchcock</em></h3>
+                <img id="loneWolfPic" src="images/lonewolf.jpg"></img>`
 };
 
 // Checks if the user clicked the button to lock it
-let buttonLocked = false;
+let nodeLocked = false;
 
 main();
 
@@ -22,35 +34,35 @@ function main() {
 }
 
 function checkEvents() { // Displays button content
-    for (let button of buttons) {
+    for (let node of nodes) {
 
         // Checks for when the button is hovered over
-        button.addEventListener("mouseover", function() {
-            if (!buttonLocked) {
-                setButton(button);
+        node.addEventListener("mouseover", function() {
+            if (!nodeLocked) {
+                setButton(node);
             }
         });
 
         // Clears the tooltip when not being hovered over
-        button.addEventListener("mouseout", function () {
-            if (!buttonLocked) {
+        node.addEventListener("mouseout", function () {
+            if (!nodeLocked) {
                 tooltip.style.display = "none";
             }
         });
 
         // Locks the button when clicked
-        button.addEventListener("click", function() {
-            buttonLocked = true;
-            setButton(button);
+        node.addEventListener("click", function() {
+            nodeLocked = true;
+            setButton(node);
 
-            button.style.backgroundColor = "red";
+            node.style.backgroundColor = "red";
 
             // Disables this button and re-enables all other buttons
-            button.disabled = true;
-            for (let otherButton of buttons) {
-                if (otherButton !== button) {
-                    otherButton.disabled = false;
-                    otherButton.style.backgroundColor = "#007200";
+            node.disabled = true;
+            for (let otherNode of nodes) {
+                if (otherNode !== node) {
+                    otherNode.disabled = false;
+                    otherNode.style.backgroundColor = "#007200";
                 }
             }
         });
@@ -58,12 +70,12 @@ function checkEvents() { // Displays button content
 
     // Closes the tooltip whenever the user clicks anywhere else on the page
     document.addEventListener("click", function(e) {
-        if (![...buttons].includes(e.target)) {
-            buttonLocked = false;
+        if (![...nodes].includes(e.target)) {
+            nodeLocked = false;
             tooltip.style.display = "none";
 
             // Re-enables all buttons
-            for (let button of buttons) {
+            for (let button of nodes) {
                 button.disabled = false;
                 button.style.backgroundColor = "#007200";
             }
@@ -75,9 +87,10 @@ function setButton(button) { // Sets up and displays the contents of the popup
     // Chooses which content from tooltipContent to use with the button's id
     const content = tooltipContent[button.id];
     if (!content) return;
-    tooltip.textContent = content;
+    tooltip.innerHTML = content;
 
     tooltip.style.display = "block";
+    tooltip.classList.remove("right-pointer", "left-pointer");
 
     // Gets the button's position
     const rect = button.getBoundingClientRect();
@@ -85,14 +98,16 @@ function setButton(button) { // Sets up and displays the contents of the popup
     const tooltipWidth = tooltip.offsetWidth;
 
     tooltip.style.position = "absolute";
-    tooltip.style.top = button.offsetTop + "px";
+    tooltip.style.top = button.offsetTop - 15 + "px";
 
     // Displays the tooltip relative to the right of the button
     if (button.classList.contains("right")) {
-        tooltip.style.left = rect.right + window.scrollX + 10 + "px";
+        tooltip.style.left = rect.right + window.scrollX + 40 + "px";
+        tooltip.classList.add("right-pointer");
 
     // Displays the tooltip relative to the left of the button
     } else {
-        tooltip.style.left = rect.left + window.scrollX - tooltipWidth - 10 + "px";
+        tooltip.style.left = rect.left + window.scrollX - tooltipWidth - 40 + "px";
+        tooltip.classList.add("left-pointer");
     }
 }
